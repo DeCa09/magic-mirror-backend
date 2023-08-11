e405a0e2ae466a9118b163d65956e6ee0aeb9f4c
Demir Catovic <dcatovic@amazon.com>
2023-08-11 19:14:06 +0200

    first fastapi

---
+++ src/magic_mirror_backend/api/serving.py
@@ -0,0 +1,45 @@
+import json
+
+from fastapi import FastAPI # type: ignore
+
+from chatbot.llama_chatbot import LlamaChatbot
+
+
+app = FastAPI()
+
+
+# load dummy model
+llama_chatbot = LlamaChatbot()
+print(json.dumps("Dummy model is loaded."))
+print(type(llama_chatbot))
+
+@app.get("/")
+async def root() -> dict:
+    """
+    **Dummy endpoint that returns 'hello world' example.**
+
+    ```
+    Returns:
+        dict: 'hello world' message.
+    ```
+    """
+    response = {"message": "Hello World"}
+    return response
+
+
+
+@app.get("/chatbot")
+async def chatbot(user_input: str) -> dict:
+    """
+    **Endpoint that returns the chatbot response.**
+
+    ```
+    Returns:
+        dict: chatbot response message.
+    ```
+    """
+
+    llama_chatbot = LlamaChatbot()
+    response = llama_chatbot(user_input)
+
+    return {"chatbot_response": f"Hello from ze chatbot: {response}"}
